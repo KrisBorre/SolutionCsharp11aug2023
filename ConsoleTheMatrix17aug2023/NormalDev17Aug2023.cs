81e0c0cdeb2c816d7a7c61ba58d924fce9f0578d
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-08-17 09:21:13 +0200

    Numerical Recipes

---
+++ ConsoleTheMatrix17aug2023/NormalDev17Aug2023.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleTheMatrix17aug2023
+{
+    // Numerical Recipes
+
+    internal class NormalDev17Aug2023 : Random17Aug2023
+    {
+        double mu, sig;
+
+        public NormalDev17Aug2023(double mmu, double ssig, ulong i) : base(i)
+        {
+            mu = mmu;
+            sig = ssig;
+        }
+
+        public double dev()
+        {
+            double u, v, x, y, q;
+            do
+            {
+                u = doub();
+                v = 1.7156 * (doub() - 0.5);
+                x = u - 0.449871;
+                y = Math.Abs(v) + 0.386595;
+                q = Math.Sqrt(x) + y * (0.19600 * y - 0.25472 * x);
+            } while (q > 0.27597 && (q > 0.27846 || Math.Sqrt(v) > -4.0 * Math.Log(u) * Math.Sqrt(u)));
+            return mu + sig * v / u;
+        }
+    }
+}
---
+++ ConsoleTheMatrix17aug2023/Program.cs
@@ -0,0 +1,87 @@
+﻿using ConsoleTheMatrix17aug2023;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welkom bij de film The Matrix.");
+
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+        //Console.SetBufferSize(90, 30); // gooit Exception op desktop computer
+
+        int consoleWidth = 80;
+        int consoleHeight = Console.LargestWindowHeight;
+        
+        Console.CursorVisible = false;
+        Console.SetWindowSize(consoleWidth, consoleHeight);
+
+        ConsoleColor defaultForegroundColor = Console.ForegroundColor;
+        ConsoleColor defaultBackgroundColor = Console.BackgroundColor;
+
+        long ticks = DateTime.Now.Ticks;
+        ulong seed = (ulong)ticks;
+        var rangen = new NormalDev17Aug2023(0, 1, seed);
+
+        ConsoleColor[] consoleColors = { ConsoleColor.DarkMagenta, ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkGreen, ConsoleColor.DarkRed, ConsoleColor.Red, ConsoleColor.Blue };
+
+        double radiusMax = Math.Sqrt(Math.Pow(consoleHeight, 2) + Math.Pow(consoleWidth, 2)); // 84
+
+        for (ulong i = 0; i < 100_000_000_000; i++)
+        {
+            int j = Math.Abs((int)rangen.int32()) % consoleWidth;
+            int k = Math.Abs((int)rangen.int32()) % consoleHeight;
+            Console.SetCursorPosition(j, k);
+
+            double radius = Math.Sqrt(Math.Pow(k + 1, 2) + Math.Pow(j + 1, 2));
+
+            const int minimum1 = 62;
+            const int maximum1 = 400;
+
+            // Veel karakters in het begin
+            // radius klein 2 -> scaleFactor gelijk aan een getal veel groter dan 300 -> 168 / 2  = 84 
+            // radius groot 80 -> scaleFactor gelijk aan 2
+            double scaleFactor1 = (2 * radiusMax) / radius;
+            // in het begin veel vreemde cijfers dus shift is 150
+            // radius klein 2 -> shift 150
+            // radius groot 80 -> shift naar 200
+            double shift1 = 148 + radius;
+            double a1 = (scaleFactor1 * rangen.dev()) + shift1;
+            int a5 = Math.Abs((int)a1);
+            int bin1 = (a5 % (maximum1 - minimum1)) + minimum1;
+
+            char teken = Convert.ToChar(bin1);
+
+            const int minimum2 = 0;
+            int maximum2 = consoleColors.Length;
+            // Weinig kleuren in het begin
+            // radius klein 2 -> scale factor 2
+            // radius groot 80 -> scale factor 3
+            double scaleFactor2 = radius / 40.0;
+            // radius klein 2 -> shift initieel is 0
+            // radius groot 80 -> shift naar 6
+            double shift2 = (radius / 30.0) - 0.1;
+
+            double b1 = (scaleFactor2 * rangen.dev()) + shift2;
+            int b5 = Math.Abs((int)b1);
+            int index = (b5 % (maximum2 - minimum2)) + minimum2; // binning
+
+            Console.ForegroundColor = consoleColors[index];
+
+            Console.Write(teken);
+
+            System.Threading.Thread.Sleep(1); //dit getal is in milliseconden.
+        }
+
+        Console.BackgroundColor = defaultBackgroundColor;
+        Console.ForegroundColor = defaultForegroundColor;
+
+        stopwatch.Stop();
+        double totaletijd = stopwatch.Elapsed.TotalSeconds;
+        
+        Console.SetCursorPosition(0, consoleHeight / 2);
+        Console.WriteLine($"De computer tijd nodig voor deze berekening is {totaletijd} seconden.");
+
+        Console.ReadLine();
+    }
+}
