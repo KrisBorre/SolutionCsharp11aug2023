c74ffcec5c5f363d49d08c42e162da27d91fc9b4
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-14 13:59:59 +0200

    Histogram van normaal verdeling in Forms application

---
+++ WindowsFormsFramework14sep2023/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsFramework14sep2023
+{
+    public partial class Form1 : Form
+    {
+        private Chart chart;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = "Histogram van normaal verdeling";
+
+            // Add Reference -> Assemblies -> Framework -> System.Windows.Forms.DataVisualization
+
+            ChartArea chartArea = new ChartArea();
+            Series series = new Series();
+            chart = new Chart();
+
+            chartArea.Name = "ChartArea";
+            chart.ChartAreas.Add(chartArea);
+            chart.Location = new Point(66, 35);
+            chart.Name = "chart";
+            series.ChartArea = "ChartArea";
+            series.ChartType = SeriesChartType.RangeColumn;
+            //series2.ChartType = SeriesChartType.Point;
+            //series.Legend = "legend";
+            series.Name = "series";
+
+            double variantie_breed = 10.0;
+
+            const int NPTS = 200000;
+            const int N = 20; // Er worden 21 histogrambalken getoond.
+
+            const int NOVER2 = N / 2, ISCAL = 400, LLEN = 180;
+            int klim;
+            ulong idum;
+            unchecked
+            {
+                idum = (ulong)(-13);
+            }
+            double dd;
+            double x6;
+            int[] dist = new int[N + 1];
+            NormaalVerdeling_BoxMuller_14sep2023 gasdev = new NormaalVerdeling_BoxMuller_14sep2023(0.0, variantie_breed, idum);
+            for (int j = 0; j <= N; j++) dist[j] = 0;
+
+            for (int i = 0; i < NPTS; i++)
+            {
+                x6 = 0.025 * N * gasdev.Afwijking();
+                int j = (int)(x6 > 0 ? x6 + 0.5 : x6 - 0.5);
+                if ((j >= -NOVER2) && (j <= NOVER2)) ++dist[j + NOVER2];
+            }
+            //Console.WriteLine($"Normally distributed deviate of {NPTS} points with variance={variantie_breed}");
+            //Console.WriteLine("     x      p(x)        graph:");
+            //Console.WriteLine();
+
+            for (int j = 0; j <= N; j++)
+            {
+                dd = ((double)(dist[j])) / NPTS;
+                klim = (int)(ISCAL * dd);
+                if (klim > LLEN) klim = LLEN;
+                string txt = "";
+                for (int i = 0; i < klim; i++)
+                {
+                    txt += '*';
+                }
+                //Console.Write($"{j / (0.25 * N),8} {dd,9}     ");
+                //Console.WriteLine(txt);
+            }
+
+            for (int j = 0; j <= N; j++)
+            {
+                series.Points.Add(new DataPoint(j, dist[j]));
+            }
+
+            chart.Dock = DockStyle.Fill;
+            chart.Series.Add(series);
+            chart.Size = new Size(300, 300);
+            chart.TabIndex = 0;
+            chart.Text = "chart";
+
+            Controls.Add(chart);
+        }
+    }
+}
---
+++ WindowsFormsFramework14sep2023/NormaalVerdeling_Leva1992_14sep2023.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WindowsFormsFramework14sep2023
+{
+    /// <summary>
+    /// Gauss distributie met behulp van het algorithme van Leva (1992)
+    /// </summary>
+    internal class NormaalVerdeling_Leva1992_14sep2023 : Random14sep2023
+    {
+        private double mu, sig;
+
+        public NormaalVerdeling_Leva1992_14sep2023(double gemiddelde = 0, double variantie = 1, ulong zaadje = 5) : base(zaadje)
+        {
+            mu = gemiddelde;
+            sig = variantie;
+        }
+
+        public double Afwijking()
+        {
+            double u, v, x, y, q;
+            do
+            {
+                u = doub();
+                v = 1.7156 * (doub() - 0.5);
+                x = u - 0.449871;
+                y = Math.Abs(v) + 0.386595;
+                q = Math.Pow(x, 2) + y * (0.19600 * y - 0.25472 * x);
+            } while (q > 0.27597 && (q > 0.27846 || Math.Pow(v, 2) > -4.0 * Math.Log(u) * Math.Pow(u, 2)));
+            return mu + sig * v / u;
+        }
+    }
+}
