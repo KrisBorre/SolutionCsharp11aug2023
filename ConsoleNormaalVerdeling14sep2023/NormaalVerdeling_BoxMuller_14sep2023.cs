e7e3637e309752410c493e4e365351225a271603
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-14 12:59:34 +0200

    normaal verdeling (Gauss distributie)

---
+++ ConsoleNormaalVerdeling14sep2023/NormaalVerdeling_BoxMuller_14sep2023.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleNormaalVerdeling14sep2023
+{
+    /// <summary>
+    /// Gauss distributie d.m.v. Box Muller methode.
+    /// </summary>
+    internal class NormaalVerdeling_BoxMuller_14sep2023 : Random14sep2023
+    {
+        private double mu, sig;
+        private double storedval;
+
+        public NormaalVerdeling_BoxMuller_14sep2023(double gemiddelde = 0, double variantie = 1, ulong zaadje = 5) : base(zaadje)
+        {
+            mu = gemiddelde;
+            sig = variantie;
+            storedval = 0.0;
+        }
+
+        public double Afwijking()
+        {
+            double result;
+            double v1, v2, rsq, fac;
+
+            if (storedval == 0.0)
+            {
+                do
+                {
+                    v1 = 2.0 * doub() - 1.0;
+                    v2 = 2.0 * doub() - 1.0;
+                    rsq = v1 * v1 + v2 * v2;
+                } while (rsq >= 1.0 || rsq == 0.0);
+
+                fac = Math.Sqrt(-2.0 * Math.Log(rsq) / rsq);
+                storedval = v1 * fac;
+                result = mu + sig * v2 * fac;
+            }
+            else
+            {
+                fac = storedval;
+                storedval = 0.0;
+                result = mu + sig * fac;
+            }
+
+            return result;
+        }
+
+    }
+}
+
+
+
+
---
+++ ConsoleNormaalVerdeling14sep2023/Program.cs
@@ -0,0 +1,122 @@
+﻿using ConsoleNormaalVerdeling14sep2023;
+
+internal class Program
+{
+    static void HistogramBoxMuller(double variance = 1.0)
+    {
+        const int NPTS = 200000;
+        const int N = 20, NOVER2 = N / 2, ISCAL = 400, LLEN = 180;
+        int klim;
+        ulong idum;
+        unchecked
+        {
+            idum = (ulong)(-13);
+        }
+        double dd, x6;
+        int[] dist = new int[N + 1];
+        NormaalVerdeling_BoxMuller_14sep2023 gasdev = new NormaalVerdeling_BoxMuller_14sep2023(0.0, variance, idum);
+        for (int j = 0; j <= N; j++) dist[j] = 0;
+
+        for (int i = 0; i < NPTS; i++)
+        {
+            x6 = 0.025 * N * gasdev.Afwijking();
+            int j = (int)(x6 > 0 ? x6 + 0.5 : x6 - 0.5);
+            if ((j >= -NOVER2) && (j <= NOVER2)) ++dist[j + NOVER2];
+        }
+        Console.WriteLine($"Normally distributed deviate of {NPTS} points with variance={variance}");
+        Console.WriteLine("     x      p(x)        graph:");
+        Console.WriteLine();
+
+        for (int j = 0; j <= N; j++)
+        {
+            dd = ((double)(dist[j])) / NPTS;
+            klim = (int)(ISCAL * dd);
+            if (klim > LLEN) klim = LLEN;
+            string txt = "";
+            for (int i = 0; i < klim; i++)
+            {
+                txt += '*';
+            }
+            Console.Write($"{j / (0.25 * N),8} {dd,9}     ");
+            Console.WriteLine(txt);
+        }
+    }
+
+
+    static void HistogramLeva1992(double variance = 1.0)
+    {
+        const int NPTS = 200000;
+        const int N = 20, NOVER2 = N / 2, ISCAL = 400, LLEN = 180;
+        int klim;
+        ulong idum;
+        unchecked
+        {
+            idum = (ulong)(-13);
+        }
+        double dd, x6;
+        int[] dist = new int[N + 1];
+        NormaalVerdeling_Leva1992_14sep2023 gasdev = new NormaalVerdeling_Leva1992_14sep2023(0.0, variance, idum);
+        for (int j = 0; j <= N; j++) dist[j] = 0;
+
+        for (int i = 0; i < NPTS; i++)
+        {
+            x6 = 0.025 * N * gasdev.Afwijking();
+            int j = (int)(x6 > 0 ? x6 + 0.5 : x6 - 0.5);
+            if ((j >= -NOVER2) && (j <= NOVER2)) ++dist[j + NOVER2];
+        }
+        Console.WriteLine($"Normally distributed deviate of {NPTS} points with variance={variance}");
+        Console.WriteLine("     x      p(x)        graph:");
+        Console.WriteLine();
+
+        for (int j = 0; j <= N; j++)
+        {
+            dd = ((double)(dist[j])) / NPTS;
+            klim = (int)(ISCAL * dd);
+            if (klim > LLEN) klim = LLEN;
+            string txt = "";
+            for (int i = 0; i < klim; i++)
+            {
+                txt += '*';
+            }
+            Console.Write($"{j / (0.25 * N),8} {dd,9}     ");
+            Console.WriteLine(txt);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.SetWindowSize(180, 30);
+
+        double variantie_smal = 2.0;
+        double variantie_breed = 10.0;
+
+        Console.WriteLine("Normaal verdeling door middel van de traditionele Box Muller methode:");
+
+        Console.WriteLine("Een smalle Gauss distributie:\n");
+        HistogramBoxMuller(variantie_smal);
+
+        Console.Write("Druk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("Een brede Gauss distributie:\n");
+        HistogramBoxMuller(variantie_breed);
+
+        Console.Write("Druk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("Normaal verdeling met algorithme van Leva (1992):");
+
+        Console.WriteLine("Een smalle Gauss distributie:\n");
+        HistogramLeva1992(variantie_smal);
+
+        Console.Write("Druk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("Een brede Gauss distributie:\n");
+        HistogramLeva1992(variantie_breed);
+
+    }
+}
