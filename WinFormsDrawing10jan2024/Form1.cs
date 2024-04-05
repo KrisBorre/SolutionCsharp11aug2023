2751df619b6ab5eb9d8a2d2220f99b171e008707
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-04-05 17:05:45 +0200

    Reflection

--- WinFormsDrawing10jan2024/Form1.cs
+++ WinFormsDrawing10jan2024/Form1.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Drawing.Drawing2D;
+using System.Reflection;
 
 namespace WinFormsDrawing10jan2024
 {
@@ -95,6 +97,25 @@ namespace WinFormsDrawing10jan2024
             // Set the size of the image and add it onto the form so it renders.
             picture.SizeMode = PictureBoxSizeMode.AutoSize;
             this.Controls.Add(picture);
+
+            // loop through the assemblies that this app references 
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly so we can read its details
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                // declare a variable to count the number of methods
+                int methodCount = 0;
+
+                // loop through all the types in the assembly 
+                foreach (var t in a.DefinedTypes)
+                {
+                    // add up the counts of methods 
+                    methodCount += t.GetMethods().Count();
+                }
+                // output the count of types and their methods
+                Debug.WriteLine($"{a.DefinedTypes.Count():N0} types with {methodCount:N0} methods in {r.Name} assembly.");
+            }
         }
     }
 }
